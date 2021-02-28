e774df40dbe9b211c3dbaada4ebbc1ed974a9bf1
ArthurDelamare <arthur.delamare1@viacesi.fr>
2021-02-28 17:46:25 +0100

    continue popup

--- vue/popup.js
+++ vue/popup.js
@@ -1,13 +1,26 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Canal+ Party',
+    title: 'MyParty pour Canal+',
+    users: [],
     home: true,
-    lobby: false
+    lobby: false,
+    join: false,
   },
   methods: {
-    createLobby: function () {
+    createLobby: function() {
       this.home = false;
+      this.join = false;
+      this.lobby = true;
+    },
+    joinLobby: function() {
+      this.home = false;
+      this.join = true;
+      this.lobby = false;
+    },
+    tryConnect: function() {
+      this.home = false;
+      this.join = false;
       this.lobby = true;
     },
     copyLink: function() {
